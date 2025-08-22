afe79541eefae82bf9ad91d851f9ae16a596e3c7
Miguel Roque <miguelroqueop@gmail.com>
2025-08-22 12:53:01 -0500

    back-front

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://activityhub-frontend-production.up.railway.app",
     credentials: true,
   })
 );
